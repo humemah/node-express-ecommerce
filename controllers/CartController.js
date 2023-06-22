012ea6dc57448b54946eb772f1b622ae5f22596e
Humemah <Humemah.Khalid@csquare.co>
2023-06-22 17:01:45 +0500

    Initial commit

--- controllers/CartController.js
+++ controllers/CartController.js
@@ -1,28 +1,46 @@
+const Cart = require('../models/Cart');
+const Product = require('../models/Product');
 const { createSuccessResponse, createErrorResponse } = require('../responseUtils');
-const cartService = require('../services/CartServices');
 
-const addToCart = async (req, res) => {
+exports.getCarts = async (req, res) => {
   try {
-    const productName = req.body.productName;
-    const userId = req.user.userId;
-
-    const message = await cartService.addToCart(productName, userId);
-    res.json(createSuccessResponse(message));
-  } catch (err) {
-    res.status(500).json(createErrorResponse(err.message || 'Failed to add product to cart'));
-  }
-};
-
-const getCarts = async (req, res) => {
-  try {
-    const carts = await cartService.getCarts();
+    const carts = await Cart.find();
     res.json(createSuccessResponse(carts));
   } catch (err) {
     res.status(500).json(createErrorResponse(err.message || 'Failed to fetch carts'));
   }
 };
 
-module.exports = {
-  addToCart,
-  getCarts
+exports.addToCart = async (req, res) => {
+  try {
+    const productName = req.body.productName;
+    const userId = req.user.userId;
+
+    const product = await Product.findOne({ name: productName });
+    if (!product) {
+      throw new Error('Product not found');
+    }
+
+    let cart = await Cart.findOne({ userId });
+    if (!cart) {
+      cart = new Cart({ userId: userId, items: [] });
+    } else if (!cart.items) {
+      cart.items = [];
+    }
+
+    const item = {
+      product: product._id,
+      quantity: 1,
+      price: product.price
+    };
+
+    cart.items.push(item);
+    cart.totalPrice += item.price;
+
+    await cart.save();
+
+    res.json(createSuccessResponse('Product added to cart'));
+  } catch (err) {
+    res.status(500).json(createErrorResponse(err.message || 'Failed to add product to cart'));
+  }
 };
--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -1,74 +1,58 @@
-// controllers/productController.js
+const express = require('express');
+const router = express.Router();
 const { createSuccessResponse, createErrorResponse } = require('../responseUtils');
-const productService = require('../services/ProductServices');
 const multer = require('multer');
 const upload = multer({ dest: 'uploads/' }).single('user_file');
+const Product = require('../models/Product');
 
-exports.getAllProducts = async (req, res) => {
+// Middleware function to fetch a product by ID
+async function getProduct(req, res, next) {
   try {
-    const products = await productService.getAllProducts();
+    const product = await Product.findById(req.params.id);
+    if (product == null) {
+      return res.status(404).json({ message: "Cannot find product" });
+    }
+    res.product = product;
+    next();
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+}
+
+// Get all products
+router.get('/', async (req, res) => {
+  try {
+    const products = await Product.find();
     res.json(createSuccessResponse(products));
   } catch (err) {
     res.status(500).json(createErrorResponse(err.message));
   }
-};
+});
 
-exports.searchProducts = async (req, res) => {
+// Get a specific product
+router.get('/:id', getProduct, (req, res) => {
+  res.json(createSuccessResponse(res.product));
+});
+
+// Create a new product
+router.post('/', upload, async (req, res) => {
   try {
-    const { name, price } = req.query;
-    const products = await productService.searchProducts(name, price);
-    res.json(createSuccessResponse(products));
+    const productData = {
+      name: req.body.name,
+      price: req.body.price,
+      description: req.body.description,
+      images: [req.file.filename],
+    };
+
+    const newProduct = await Product.create(productData);
+    res.status(201).json(createSuccessResponse(newProduct));
   } catch (err) {
-    res.status(500).json(createErrorResponse(err.message));
+    res.status(400).json(createErrorResponse(err.message));
   }
-};
+});
 
-exports.getProductById = async (req, res) => {
-  try {
-    // const productId = req.params.productId;
-    const productId = req.params.id;
-
-    const product = await productService.getProductById(productId);
-    res.json(createSuccessResponse(product));
-  } catch (err) {
-    res.status(500).json(createErrorResponse(err.message));
-  }
-};
-
-exports.createProduct = (req, res) => {
-  upload(req, res, async function (err) {
-    if (err) {
-      console.error(err);
-      return res.status(500).json({ message: 'File upload failed' });
-    }
-
-    try {
-      const productData = {
-        name: req.body.name,
-        price: req.body.price,
-        description: req.body.description,
-        images: [req.file.filename],
-      };
-
-      const newProduct = await productService.createProduct(productData);
-      res.status(201).json(createSuccessResponse(newProduct));
-    } catch (err) {
-      res.status(400).json(createErrorResponse(err.message));
-    }
-  });
-};
-
-exports.deleteProduct = async (req, res) => {
-  try {
-    const productId = req.params.id;
-    await productService.deleteProduct(productId);
-    res.json(createSuccessResponse({ message: "Product Deleted Successfully" }));
-  } catch (err) {
-    res.status(500).json(createErrorResponse(err.message));
-  }
-};
-
-exports.updateProduct = async (req, res) => {
+// Update a product
+router.patch('/:id', getProduct, async (req, res) => {
   try {
     const productId = req.params.id;
     const updatedData = {
@@ -76,9 +60,28 @@ exports.updateProduct = async (req, res) => {
       price: req.body.price,
       description: req.body.description,
     };
-    const updatedProduct = await productService.updateProduct(productId, updatedData);
+    const updatedProduct = await Product.findByIdAndUpdate(productId, updatedData, { new: true });
+    if (!updatedProduct) {
+      throw new Error('Product not found');
+    }
     res.json(createSuccessResponse(updatedProduct));
   } catch (err) {
     res.status(400).json(createErrorResponse(err.message));
   }
-};
+});
+
+// Delete a product
+router.delete('/:id', getProduct, async (req, res) => {
+  try {
+    const productId = req.params.id;
+    const deletedProduct = await Product.findByIdAndDelete(productId);
+    if (!deletedProduct) {
+      throw new Error('Product not found');
+    }
+    res.json(createSuccessResponse({ message: 'Product deleted successfully' }));
+  } catch (err) {
+    res.status(500).json(createErrorResponse(err.message));
+  }
+});
+
+
